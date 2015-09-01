43e1cbd422511b641aea7346ff522d7c8a658a7d
Maciej Bielecki <zyla@prati.pl>
2015-09-01 10:51:30 +0200

    Refactor and add docs

--- Main.hs
+++ Main.hs
@@ -10,41 +10,114 @@ import Control.Applicative
 import Data.List
 
 -- | Lambda calculus expression with embedded portions of pure SKI.
-data Lambda = LVar String | LApp Lambda Lambda | Lambda String Lambda | SKI SKI
+data Lambda = LVar String | LApp Lambda Lambda | Lambda String Lambda | SKI SKI deriving (Eq)
+
+instance Show Lambda where
+    showsPrec p (LApp (LApp (LVar "(<*>)") x) y) =
+        showParen (p > 2) $ showsPrec 2 x . showString " <*> " . showsPrec 3 y
+    showsPrec p (LApp (LVar "(<*>)") x) = showParen True $ showsPrec 2 x . showString " <*>"
+    showsPrec p (LApp (SKI f) x) = showParen (p > 2) $ showsPrec 2 f . showsPrec 3 x
+    showsPrec p (LApp f x) = showParen (p > 2) $ showsPrec 2 f . showString " " . showsPrec 3 x
+    showsPrec _ (LVar s) = showString s
+    showsPrec p (Lambda v f) =
+        let (vars, body) = dissectLambda f in
+        showParen (p > 2) $ showString ("\\" ++ intercalate " " (v:vars) ++  " -> ")
+                          . showsPrec 2 body
+      where
+        dissectLambda (Lambda v f) = let (vars, body) = dissectLambda f in (v:vars, body)
+        dissectLambda body = ([], body)
+    showsPrec p (SKI ski) = showsPrec p ski
 
 -- | SKI expression
 data SKI = SApp SKI SKI | S | K | I deriving (Eq)
+
+instance Show SKI where
+    showsPrec p (SApp f x) = showParen (p > 2) $ showsPrec 2 f . showsPrec 3 x
+    showsPrec _ S = showString "S"
+    showsPrec _ K = showString "K"
+    showsPrec _ I = showString "I"
 
 -- | Helpers for pattern matching on nested applications - much more readable.
 pattern A1 f x = SApp f x
 pattern A2 f x y = SApp (SApp f x) y
 pattern A3 f x y z = SApp (SApp (SApp f x) y) z
 
-skiSimpl :: Lambda -> Lambda
-skiSimpl (Lambda v f@(Lambda _ _)) =
-    case skiSimpl f of
+-- | One step of lambda -> SKI conversion.
+-- WARNING: This is a partial function (errors out on free variables).
+lambdaToSKIStep :: Lambda -> Lambda
+lambdaToSKIStep (Lambda v f@(Lambda _ _)) =
+    case lambdaToSKIStep f of
         SKI f' -> SKI $ SApp K f'
         f' -> Lambda v f'
 
-skiSimpl (Lambda v (LApp f x)) = LApp (LApp (SKI S) (Lambda v f)) (Lambda v x)
+lambdaToSKIStep (Lambda v (LApp f x)) = LApp (LApp (SKI S) (Lambda v f)) (Lambda v x)
 
-skiSimpl (Lambda v (LVar s))
+lambdaToSKIStep (Lambda v (LVar s))
     | v == s    = SKI I
     | otherwise = LApp (SKI K) (LVar s)
 
-skiSimpl (Lambda v (SKI f)) = SKI $ SApp K f
+lambdaToSKIStep (Lambda v (SKI f)) = SKI $ SApp K f
 
-skiSimpl (LVar v) = error $ "free variable " ++ v
+lambdaToSKIStep (LVar v) = error $ "free variable " ++ v
 
-skiSimpl (LApp f x) =
-    case (skiSimpl f, skiSimpl x) of
+lambdaToSKIStep (LApp f x) =
+    case (lambdaToSKIStep f, lambdaToSKIStep x) of
         (SKI f', SKI x') -> SKI $ SApp f' x'
         (f', x') -> LApp f' x'
 
-skiSimpl (SKI ski) = SKI ski
+lambdaToSKIStep (SKI ski) = SKI ski
 
-skiSimpls (SKI ski) = [SKI ski]
-skiSimpls l = l : skiSimpls (skiSimpl l)
+-- Iterate a given function until output equals input, i.e. a fixed point.
+toFixedPoint :: Eq a => (a -> a) -> a -> [a]
+toFixedPoint f x = x : xs
+  where
+    xs = if x == next then [] else toFixedPoint f next
+    next = f x
+
+-- | Convert lambda to SKI (without simplifications).
+-- Returns a tuple of (conversion steps, final SKI).
+lambdaToSKI :: Lambda -> ([Lambda], SKI)
+lambdaToSKI (SKI end) = ([], end)
+lambdaToSKI l =
+    let (steps, expr) = lambdaToSKI $ lambdaToSKIStep l
+    in (l : steps, expr)
+
+-- | One step of SKI simplitication
+simplSKIStep :: SKI -> SKI
+simplSKIStep (A1 I x) = x
+simplSKIStep (A2 K _ x) = x
+simplSKIStep (A2 S (A1 K f) (A1 K x)) = A1 K (A1 f x)
+simplSKIStep (A1 f x) = A1 (simplSKIStep f) (simplSKIStep x)
+simplSKIStep ski = ski
+
+-- | Render a SKI expression in Haskell
+backToLambda :: SKI -> Lambda
+backToLambda (SApp f x) = LApp (backToLambda f) (backToLambda x)
+backToLambda S = LVar "(<*>)"
+backToLambda K = LVar "pure"
+backToLambda I = LVar "id"
+
+-- | Convert a lambda expression to simplified SKI, returning all intermediate steps.
+--
+-- Returns a tuple of 3 elements:
+--  * lambda-to-SKI steps
+--  * SKI simplification steps
+--  * final SKI expression converted back to lambda
+process :: Lambda -> ([Lambda], [SKI], Lambda)
+process input = (lambdaSteps, skiSteps, finalLambda)
+  where
+    (lambdaSteps, ski) = lambdaToSKI input
+    skiSteps = toFixedPoint simplSKIStep ski
+    finalLambda = backToLambda $ last skiSteps
+
+main = do
+    let input = flip'
+    let (lambdaSteps, skiSteps, finalLambda) = process input
+    mapM_ print lambdaSteps
+    mapM_ print skiSteps
+    print finalLambda
+
+-- examples
 
 id' :: Lambda
 id' = Lambda "x" $ LVar "x"
@@ -57,54 +130,6 @@ compose = Lambda "f" $ Lambda "g" $ Lambda "x" $ LApp (LVar "f") (LApp (LVar "g"
 
 flip' :: Lambda
 flip' = Lambda "f" $ Lambda "x" $ Lambda "y" $ LApp (LApp (LVar "f") (LVar "y")) (LVar "x")
-
-simplSKI :: SKI -> SKI
-simplSKI (A1 I x) = x
-simplSKI (A2 K _ x) = x
-simplSKI (A2 S (A1 K f) (A1 K x)) = A1 K (A1 f x)
-simplSKI (A1 f x) = A1 (simplSKI f) (simplSKI x)
-simplSKI ski = ski
-
-backToLambda :: SKI -> Lambda
-backToLambda (SApp f x) = LApp (backToLambda f) (backToLambda x)
-backToLambda S = LVar "(<*>)"
-backToLambda K = LVar "pure"
-backToLambda I = LVar "id"
-
-instance Show Lambda where
-    showsPrec p (LApp (LApp (LVar "(<*>)") x) y) =
-        showParen (p > 2) $ showsPrec 2 x . showString " <*> " . showsPrec 3 y
-    showsPrec p (LApp (LVar "(<*>)") x) = showParen True $ showsPrec 2 x . showString " <*>"
-    showsPrec p (LApp (SKI f) x) = showParen (p > 2) $ showsPrec 2 f . showsPrec 3 x
-    showsPrec p (LApp f x) = showParen (p > 2) $ showsPrec 2 f . showString " " . showsPrec 3 x
-    showsPrec _ (LVar s) = showString s
-    showsPrec p (Lambda v f) = let (vars, body) = dissectLambda (Lambda v f) in
-        showParen (p > 2) $ showString ("\\" ++ intercalate " " vars ++  " -> ") . showsPrec 2 body
-    showsPrec p (SKI ski) = showsPrec p ski
-
-
-dissectLambda (Lambda v f) = let (vars, body) = dissectLambda f in (v:vars, body)
-dissectLambda body = ([], body)
-
-
-instance Show SKI where
-    showsPrec p (SApp f x) = showParen (p > 2) $ showsPrec 2 f . showsPrec 3 x
-    showsPrec _ S = showString "S"
-    showsPrec _ K = showString "K"
-    showsPrec _ I = showString "I"
-
-ski l = let SKI expr = last (skiSimpls l) in expr
-
-whileNE [] = []
-whileNE [x] = [x]
-whileNE (x:y:xs)
-    | x == y = [x]
-    | otherwise = x : whileNE (y:xs)
-
-main =
-    let input = flip'
-        solutions = whileNE $ iterate simplSKI $ ski input
-    in mapM_ print (skiSimpls input) >> mapM_ print solutions >> print (backToLambda $ last solutions)
 
 -- Random shit
 
